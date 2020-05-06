3c3fd31d97b9dcf2a6b2eb2dd4102383b0a56d86
SnikkiJoker <64868561+SnikkiJoker@users.noreply.github.com>
2020-05-06 22:56:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ bot.on('message', message=>{
    // F/ bruh / bruh moment counter
    ////////////////////////////////////////////////////////////////
    let args = message.content.substring(PREFIX.length).split(" ");
-   switch(ags[0]){
+   switch (ags[0]) {
    // bruh counter 
    case "bruh": 
        var bruh_send =("this is a bruh moment nº  " +bruh);
