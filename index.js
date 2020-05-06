b4469cdd37f81c4151696732f88e276dd2c94994
SnikkiJoker <64868561+SnikkiJoker@users.noreply.github.com>
2020-05-06 21:01:20 +0100

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+const discord = require('discord.js');
+const bot = new discord.Client
+    
+const PREFIX = " ' ";
+
+bot.on('ready', () =>{
+    Console.log('this bot is online');
+})
+bot.on('message', msg=>{
+    if(msg.content == "HELLO"){
+        msg.reply('hello there');
+    }    
+})
+
+
+bot.login(process.env.token);
