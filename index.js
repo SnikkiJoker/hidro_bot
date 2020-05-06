898a7c8452c20b538acbf573e1d11b4cf2120ebf
SnikkiJoker <64868561+SnikkiJoker@users.noreply.github.com>
2020-05-06 23:29:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,38 +11,43 @@ const PREFIX = '!h';
 
 
 bot.on('message', message=>{
+    // let args- responds to !halo uses cases to determine get "alo"
+   let args = message.content.substring(PREFIX.length).split(" ");
+   switch (args[0]) {
 
+   case "":
+
+            break;
+
+   case "f":
+
+           break;
+
+   }
     //sees "hello there" replies "general kenobi, you are a bold one" 
     if(message.content ===  "hello there"){
-        message.reply('general kenobi, you are a bold one');
+        client.channels.get("bot-spam").send("general kenobi, you are a bold one");
+        // message.reply('general kenobi, you are a bold one');
    }
 
    // F/ bruh / bruh moment counter
    ////////////////////////////////////////////////////////////////
-   let args = message.content.substring(PREFIX.length).split(" ");
-   switch (args[0]) {
-   // bruh counter 
-   case "bruh": 
-       var bruh_send =("this is a bruh moment nº  " +bruh);
-       message.reply(bruh_send);
-       bruh = bruh +1
-       break;
-
-   case "f":
-       // F counter
-       var F_send =("f to pay respeccts(nº "+f_momenth+")" );
-           message.reply(F_send);
-           f_moment = f_moment +1
-           break;
-
-   }
-  if(message.content === "bruh moment"){
+    if(message.content === "bruh moment"){
        var bruh_send =("this is a bruh moment nº  "+bruh );
            message.reply(bruh_send);
            bruh = bruh +1
     }
+    if(message.content === "bruh"){
+        var bruh_send =("this is a bruh moment nº  "+bruh );
+            message.reply(bruh_send);
+            bruh = bruh +1
+     }
+     if(message.content === "f"){
+            var F_send =("f to pay respeccts(nº "+f_momenth+")" );
+           message.reply(F_send);
+           f_moment = f_moment +1
+     }
    //////////////////////////////////////////////////////////////////
-
 })
 
 
