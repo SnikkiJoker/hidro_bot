8984e858ad9b8c4135887dee3f19c00daae1c965
SnikkiJoker <64868561+SnikkiJoker@users.noreply.github.com>
2020-05-06 22:34:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 const discord = require('discord.js');
 const bot = new discord.Client();
 
-const PREFIX = " ' ";
 
 bot.on('ready', () =>{
     console.log('this bot is online');
 })
+var bruh = 150 
+var f_moment= 33
+const PREFIX = '!';
+
+
 bot.on('message', msg=>{
-    if(msg.content == "HELLO"){
-        msg.client.channel('hello there');
-    }    
+
+    //sees "hello there" replies "general kenobi, you are a bold one" 
+    if(message.content ===  "hello there"){
+        message.reply('general kenobi, you are a bold one');
+   }
+
+   // F/ bruh / bruh moment counter
+   ////////////////////////////////////////////////////////////////
+   switch (args[0]){
+   // bruh counter 
+   case "bruh": 
+       var bruh_send =("this is a bruh moment nº  " +bruh);
+       message.reply(bruh_send);
+       bruh = bruh +1
+       break;
+
+   case "f":
+       // F counter
+       var F_send =("f to pay respeccts(nº "+bruh+")" );
+           message.reply(F_send);
+           f_moment = f_moment +1
+           return;
+
+   }
+  if(message.content === "bruh moment"){
+       var bruh_send =("this is a bruh moment nº  "+bruh );
+           message.reply(bruh_send);
+           bruh = bruh +1
+  }
+   //////////////////////////////////////////////////////////////////
+
 })
 
 
