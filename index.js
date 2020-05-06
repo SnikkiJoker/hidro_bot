a24a917a1825393c0dde2c2ac6188fd18cd4467d
SnikkiJoker <64868561+SnikkiJoker@users.noreply.github.com>
2020-05-06 21:33:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,17 @@
 const discord = require('discord.js');
 const bot = new discord.Client();
 
-var bruh = 103 
-var f_moment= 1
-const PREFIX = '!';
 
+const PREFIX = " ' ";
 
-bot.on('ready',  () =>{
-    console.log("this bot is online");
+bot.on('ready', () =>{
+    Console.log('this bot is online');
 })
-
-bot.on('message', message=>{
-    
-    
-    //sees "hello there" replies "general kenobi, you are a bold one" 
-    if(message.content ===  "hello there"){
-         message.reply('general kenobi, you are a bold one');
-    }
-
-    // F/ bruh / bruh moment counter
-    ////////////////////////////////////////////////////////////////
-    switch (args[0]){
-    // bruh counter 
-    case "bruh": 
-        var bruh_send =("this is a bruh moment nº  " +bruh);
-        message.reply(bruh_send);
-        bruh = bruh +1
-        break;
-
-    case "f":
-        // F counter
-        var F_send =("f to pay respeccts(nº "+bruh+")" );
-            message.reply(F_send);
-            f_moment = f_moment +1
-            return;
-
-    }
-   if(message.content === "bruh moment"){
-        var bruh_send =("this is a bruh moment nº  "+bruh );
-            message.reply(bruh_send);
-            bruh = bruh +1
-   }
-    //////////////////////////////////////////////////////////////////
-
-    let args = message.content.substring(PREFIX.length).split(" ");
-    switch(args[0]){
-        case 'no':
-            message.reply('u lmao')
-            break;
-            
+bot.on('message', msg=>{
+    if(msg.content == "HELLO"){
+        msg.reply('hello there');
     }    
 })
 
 
 bot.login(process.env.token);
-
-
-
-
-
-
-
-
-
