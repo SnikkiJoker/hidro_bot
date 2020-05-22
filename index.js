0bd870ac5335919e7e20c7ef2ab285d53e7686f2
SnikkiJoker <64868561+SnikkiJoker@users.noreply.github.com>
2020-05-22 20:04:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -50,13 +50,14 @@ bot.on('message', message=>{        //send direct messages said by the user
 
 
    // grovvy extra commands
-   if (message.content=="-s/s"){
+   if (message.content === "-s/s"){
        message.delete(1000);
     message.channel.send("shuffle").then(d_msg =>{d_msg.delete(1000);  });
+
     //message.channel.send("-skip") 
   }
 
-  
+
    // F/ bruh / bruh moment counter
    ////////////////////////////////////////////////////////////////
     if(message.content === "bruh moment"){
