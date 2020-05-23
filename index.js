424916c82cd4b3156a5b52c843b6c3688f80d931
SnikkiJoker <64868561+SnikkiJoker@users.noreply.github.com>
2020-05-23 22:38:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -51,11 +51,9 @@ bot.on('message', message=>{        //send direct messages said by the user
 
    // grovvy extra commands
    if (message.content==="delete"){
-    message.delete({  timeout:1000  });
-
-     
-     
-    //message.channel.send("-skip") 
+    message.delete({timeout:1000  });
+    message.channel.send("-skip");
+    message.delete.({timeout: 1000})
   }
 
 
