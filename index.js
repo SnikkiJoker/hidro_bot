25f7f2c0cd7bb64d3eab163d329396cca188c164
Your Name <you@example.com>
2020-09-10 22:37:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ var random01 = Math.random();
          
         }else{ //pogs for nsfw
           switch(random01){
-            case(randomo1<(1/3)):
+            case(random01<(1/3)):
               message.channel.send("<:louispog:713515823532343377> it do be a  poggers  moment "+
               "https://media.discordapp.net/attachments/704687081020063777/751800084521287710/ezgif-7-f1cdf3368972.gif");
             break;
